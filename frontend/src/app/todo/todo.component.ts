e76e3bc3b6f23a0554e784b832cb8021ae94c83f
Max Stein <mhs8558@rit.edu>
2024-11-25 13:50:18 -0500

    last comment

--- frontend/src/app/todo/todo.component.ts
+++ frontend/src/app/todo/todo.component.ts
@@ -66,14 +66,6 @@ export class TodoComponent implements OnInit {
   }
 
   //toggleComplete
-  async toggleComplete(todo: Todo) {
-    try {
-      const updatedTodo = await this.todoService.updateTodo(todo.id, !todo.completed);
-      todo.completed = updatedTodo.completed;
-    } catch (error) {
-      console.error('Error updating todo:', error);
-    }
-  }
 
   //deleteTodo
 
